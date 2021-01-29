5f0d42f961397ea923b1b8d82127ce3a85500a4a
haru <haruki6180@gmail.com>
2021-01-29 11:05:53 +0900

    ディレクトリ名変更

--- mysite/twitter/urls.py
+++ mysite/twitter/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from .views import IndexView,UserCreateView,HomeView
 
-app_name = 'polls'
+app_name = 'twitter'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('create/', UserCreateView.as_view(), name='create'),
--- mysite/twitter/views.py
+++ mysite/twitter/views.py
@@ -2,26 +2,25 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, FormView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib.auth import login, authenticate
 
 
 class IndexView(TemplateView):
-    template_name = "polls/signup.html"
+    template_name = "twitter/signup.html"
 
 # Create your views here.
 
 class UserCreateView(FormView):
     form_class = UserCreationForm
-    template_name = 'polls/create.html'
-    success_url = reverse_lazy('polls:home')
+    template_name = 'twitter/create.html'
+    success_url = reverse_lazy('twitter:home')
     def form_valid(self, form):
         print(self.request.POST['next'])
         if self.request.POST['next'] == 'back':
-            return render(self.request, 'polls/create.html', {'form': form})
+            return render(self.request, 'twitter/create.html', {'form': form})
         elif self.request.POST['next'] == 'confirm':
-            return render(self.request, 'polls/create_confirm.html', {'form': form})
+            return render(self.request, 'twitter/create_confirm.html', {'form': form})
         elif self.request.POST['next'] == 'regist':
             form.save()
             # 認証
@@ -34,8 +33,7 @@ class UserCreateView(FormView):
             return super().form_valid(form)
         else:
             # 通常このルートは通らない
-            return redirect(reverse_lazy('polls:home'))
+            return redirect(reverse_lazy('twitter:home'))
 
-class HomeView(TemplateView):
-    template_name = "polls/home.html"
+
 
