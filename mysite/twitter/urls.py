21deddf8d9c721e4f34c7c1bd6d90d1b32076c03
haru <haruki6180@gmail.com>
2021-01-30 12:47:01 +0900

    コミットしました

--- mysite/twitter/urls.py
+++ mysite/twitter/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import IndexView,UserCreateView,HomeView
+from .views import IndexView,UserInputView,UserConfirmView,UserCreateView,HomeView
 
 app_name = 'twitter'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('input/', UserInputView.as_view(), name='input'),
+    path('confirm/', UserConfirmView.as_view(), name='confirm'),
     path('create/', UserCreateView.as_view(), name='create'),
     path('home/', HomeView.as_view(), name='home'),
 ]
--- mysite/twitter/views.py
+++ mysite/twitter/views.py
@@ -1,39 +1,55 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, FormView
+from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib.auth import login, authenticate
 
 
-class IndexView(TemplateView):
+class IndexView(generic.TemplateView):
     template_name = "twitter/signup.html"
 
 # Create your views here.
 
-class UserCreateView(FormView):
+class UserInputView(generic.FormView):
     form_class = UserCreationForm
-    template_name = 'twitter/create.html'
+    template_name = 'twitter/input.html'
+    def form_valid(self, form):
+            return render(self.request, 'twitter/input.html', {'form': form})
+
+
+        
+
+class UserConfirmView(generic.FormView):
+    form_class = UserCreationForm
+    def form_valid(self, form):
+        return render(self.request, 'twitter/confirm.html', {'form': form})
+    def form_invalid(self, form):
+        return render(self.request, 'twitter/input.html', {'form': form})
+    
+
+class UserCreateView(generic.CreateView):
+    form_class = UserCreationForm
     success_url = reverse_lazy('twitter:home')
     def form_valid(self, form):
-        print(self.request.POST['next'])
-        if self.request.POST['next'] == 'back':
-            return render(self.request, 'twitter/create.html', {'form': form})
-        elif self.request.POST['next'] == 'confirm':
-            return render(self.request, 'twitter/create_confirm.html', {'form': form})
-        elif self.request.POST['next'] == 'regist':
-            form.save()
-            # 認証
-            user = authenticate(
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password1'],
-            )
-            # ログイン
-            login(self.request, user)
-            return super().form_valid(form)
-        else:
-            # 通常このルートは通らない
-            return redirect(reverse_lazy('twitter:home'))
+        form.save()
+        # 認証
+        user = authenticate(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+        )
+        # ログイン
+        login(self.request, user)
+        return super().form_valid(form)
+    def form_invalid(self, form):
+        return render(self.request, 'twitter/input.html', {'form': form})
+
+
+class HomeView(LoginRequiredMixin,generic.TemplateView):
+    template_name = "twitter/home.html"
+    login_url = '/'
 
 
 
