c8e3e53a88a717424e6c22889c26866f14fb5293
haru <haruki6180@gmail.com>
2021-02-17 14:02:43 +0900

    adminでの表示を変更

--- mysite/twitter/models.py
+++ mysite/twitter/models.py
@@ -6,12 +6,16 @@ from django.contrib.auth import get_user_model
 class Tweet(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,default="")
     text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.text
 
 class FriendShip(models.Model):
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followee_friendships')
     followee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower_friendships')
     class Meta:
         unique_together = ('follower', 'followee')
+    def __str__(self):
+        return (self.follower,"⇔",self.followee)
 
 
 
